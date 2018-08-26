bd06956fe9a6b98be69aac5e42323c4c2420485d
Victor Barbaros <barbarosvictor@yahoo.com>
2018-08-26 17:12:02 -0400

    add weather display option;

--- app/views.py
+++ app/views.py
@@ -1,7 +1,13 @@
-import feedparser
+from app import app
+
 from flask import render_template
 from flask import request
-from app import app
+
+import feedparser
+import json
+import urllib2
+import urllib
+
 
 RSS_FEEDS = {
     'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
@@ -9,25 +15,55 @@ RSS_FEEDS = {
     'fox': 'http://feeds.foxnews.com/foxnews/latest',
     'iol': 'http://www.iol.co.za/cmlink/1.640'
     }
-    
 
-# @app.route("/")
-# #@app.route("/bbc")
-# def bbc():
-#     return get_news('bbc')
+DEFAULTS = {'publication':'bbc', 'city': 'Montreal,CA'}
 
-# @app.route("/cnn")
-# def cnn():
-#     return get_news('cnn')
+WEATHER_URL = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=97cdbe13097f09f5a3334076e1d5eca4'
 
 @app.route("/")
-def get_news():
-    query = request.args.get("publication")
+def home():
+    # get customized headlines, based on user input or default
+    publication = request.args.get('publication')
+    if not publication:
+        publication = DEFAULTS['publication']
+    articles = get_news(publication)
+    # get customized weather based on user input or default
+    city = request.args.get('city')
+    if not city:
+        city = DEFAULTS['city']
+    weather = get_weather(city)
+    return render_template("home.html", articles=articles, weather=weather)
+
+def get_news(query):
     if not query or query.lower() not in RSS_FEEDS:
-        publication = "bbc"
+        publication = DEFAULTS['publication']
     else:
         publication = query.lower()
     feed = feedparser.parse(RSS_FEEDS[publication])
-    return render_template('home.html', articles=feed['entries'])
+    return feed['entries']
+
+
+def get_weather(query):
+    query = urllib.quote(query)
+    url = WEATHER_URL.format(query)
+    data = urllib2.urlopen(url).read()
+    parsed = json.loads(data)
+    weather = None
+    
+    if parsed.get('weather'):
+        weather = {
+        'description': parsed['weather'][0]['description'],
+        'temperature': parsed['main']['temp'],
+        'city': parsed['name']
+        }
+    return weather
+
+
+
+
+
+
+
+
 
 
