99e5bc2d60e95e68d40d4c0d604b6ce4b9139081
Victor Barbaros <barbarosvictor@yahoo.com>
2018-08-26 16:22:16 -0400

    Refactored links, added href and search input field;

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 import feedparser
 from flask import render_template
+from flask import request
 from app import app
 
 RSS_FEEDS = {
@@ -10,21 +11,23 @@ RSS_FEEDS = {
     }
     
 
-@app.route("/")
-@app.route("/bbc")
-def bbc():
-    return get_news('bbc')
+# @app.route("/")
+# #@app.route("/bbc")
+# def bbc():
+#     return get_news('bbc')
 
-@app.route("/cnn")
-def cnn():
-    return get_news('cnn')
+# @app.route("/cnn")
+# def cnn():
+#     return get_news('cnn')
 
 @app.route("/")
-@app.route("/<publication>")
-def get_news(publication="bbc"):
+def get_news():
+    query = request.args.get("publication")
+    if not query or query.lower() not in RSS_FEEDS:
+        publication = "bbc"
+    else:
+        publication = query.lower()
     feed = feedparser.parse(RSS_FEEDS[publication])
-    # first_article = feed['entries'][0]
-    # return render_template('home.html', article=first_article)
     return render_template('home.html', articles=feed['entries'])
 
 
