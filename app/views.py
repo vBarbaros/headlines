557658925e7df4a64bbd3614fb5454f98f3ecd69
Victor Barbaros <barbarosvictor@yahoo.com>
2018-08-26 21:12:44 -0400

    Added currency options

--- app/views.py
+++ app/views.py
@@ -16,9 +16,14 @@ RSS_FEEDS = {
     'iol': 'http://www.iol.co.za/cmlink/1.640'
     }
 
-DEFAULTS = {'publication':'bbc', 'city': 'Montreal,CA'}
+DEFAULTS = {
+    'publication':'bbc', 'city': 'Montreal,CA',
+    'currency_from': 'CAD', 'currency_to': 'USD'
+    }
 
 WEATHER_URL = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=97cdbe13097f09f5a3334076e1d5eca4'
+CURRENCY_URL = 'https://openexchangerates.org//api/latest.json?app_id=7c049d68cc6b4274970cca63852e1e99'
+
 
 @app.route("/")
 def home():
@@ -32,7 +37,19 @@ def home():
     if not city:
         city = DEFAULTS['city']
     weather = get_weather(city)
-    return render_template("home.html", articles=articles, weather=weather)
+
+    # get customized currency based on user input or default
+    currency_from = request.args.get("currency_from")
+    if not currency_from:
+        currency_from = DEFAULTS['currency_from']
+    currency_to = request.args.get("currency_to")
+    if not currency_to:
+        currency_to = DEFAULTS['currency_to']
+    rate = get_rate(currency_from, currency_to)
+    return render_template(
+        "home.html", articles=articles, weather=weather, 
+        currency_from=currency_from, currency_to=currency_to, rate=rate
+        )
 
 def get_news(query):
     if not query or query.lower() not in RSS_FEEDS:
@@ -54,9 +71,17 @@ def get_weather(query):
         weather = {
         'description': parsed['weather'][0]['description'],
         'temperature': parsed['main']['temp'],
-        'city': parsed['name']
+        'city': parsed['name'],
+        'country': parsed['sys']['country']
         }
     return weather
+
+def get_rate(frm, to):
+    all_currency = urllib2.urlopen(CURRENCY_URL).read()
+    parsed = json.loads(all_currency).get('rates')
+    frm_rate = parsed.get(frm.upper())
+    to_rate = parsed.get(to.upper())
+    return to_rate/frm_rate
 
 
 
